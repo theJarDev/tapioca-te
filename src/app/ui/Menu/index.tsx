401b112dd95e580be18cc73f686ea878c01cc4fb
Christopher Gonzalez <christopher53552@gmail.com>
2023-12-20 02:38:28 -0600

    fix arrow icon

--- src/app/ui/Menu/index.tsx
+++ src/app/ui/Menu/index.tsx
@@ -18,7 +18,11 @@ export default function Menu({
                     {children}
                 </div>
                 
-                {home && <Link href="/menu" className='z-20 -mt-8  py-2 px-6 text-center text-black font-semibold bg-[#FFC6C8] shadow-lg rounded' > {"Ver Todo 🡵"} </Link>}
+                {home && <Link href="/menu" className='z-20 flex items-center -mt-8  py-2 px-6 text-center text-black font-semibold bg-[#FFC6C8] shadow-lg rounded' > {"Ver Todo"} <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-arrow-up-right" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                <path d="M17 7l-10 10" />
+                <path d="M8 7l9 0l0 9" />
+                </svg></Link>}
             </section>
         </>
     )
